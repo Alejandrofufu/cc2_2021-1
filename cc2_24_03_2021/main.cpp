acf5aa5ffe62eca2584f1fe12f6b6a9a0c3a3540
Alejandro <borsal_ino@hotmail.com>
2021-03-24 19:07:18 -0500

    clase 24/03

--- cc2_24_03_2021/main.cpp
+++ cc2_24_03_2021/main.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    /*
     int a = 64;
-    cout<<sizeof(a);
+    cout<<sizeof(a);*/
+
+    int a = 5,*p=&a;
+    int &c=a;
+    cout<<c;
+    //int &c{a};
+    int *&q = p;
+    *q = 7;
+    cout<<"\n"<<c;
+    cout<<"\n"<<q+2;
+    cout<<"\n"<<*(q+2);
+    cout<<"\n"<<&(*(q+2));
     
     return 0;
 }
