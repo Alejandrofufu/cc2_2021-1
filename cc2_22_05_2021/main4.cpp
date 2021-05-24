228658668434afe9b4bab3adb10201a5ca3991a8
Alejandro <borsal_ino@hotmail.com>
2021-05-24 17:22:54 -0500

    tarea de areas

--- cc2_22_05_2021/main4.cpp
+++ cc2_22_05_2021/main4.cpp
@@ -2,36 +2,59 @@
 #include <cmath>
 using namespace std;
 
-float circulo(){
-    float a=0;
-    cout<<"escribir el radio: ";cin>>a;
-    return (3.141592)*a*a;
+int circulo(int * inicio){
+    cout<<"circulo:";
+    float a=(float)*inicio;
+    cout<<"\nEl radio: "<<a;
+    cout<<"\nEl area es: "<<(3.141592)*a*a<<"\n\n";
+    return 1;
 }
-float cuadrado(){
-    float a=0;
-    cout<<"ingrese el lado: ";cin>>a;
-    return a*a;
+int cuadrado(int * inicio){
+    cout<<"cuadrado:";
+    float a=(float)*inicio;
+    cout<<"\nEl lado: "<<a;
+    cout<<"\nEl area: "<<a*a<<"\n\n";
+    return 1;
 }
-float triangulo(){
-    float a=0,b=0,c=0;
-    cout<<"ingrese un lado: ";cin>>a;
-    cout<<"ingrese un segundo lado: ";cin>>b;
-    cout<<"inglrese un tercer lado: ";cin>>c;
+int triangulo(int * inicio){
+    cout<<"Triangulo: ";
+    float a=(float)*inicio;inicio++;
+    float b=(float)*inicio;inicio++;
+    float c=(float)*inicio;
+    cout<<"\nPrimer lado lado: "<<a;
+    cout<<"\nsegundo lado: "<<b;
+    cout<<"\ntercer lado: "<<c;
     float sp = (a+b+c)/2;
-    return sqrt(sp*(sp-a)*(sp-b)*(sp-c));
+    cout<<"\narea: "<<sqrt(sp*(sp-a)*(sp-b)*(sp-c))<<"\n\n";
+    return 3;
 }
-float rectangulo(){
-    float a=0,b=0;
-    cout<<"ingrese largo: ";cin>>a;
-    cout<<"ingrese ancho: ";cin>>b;
-    return a*b;
+
+int  rectangulo(int * inicio){
+    cout<<"rectangulo: ";
+    float a=*inicio;inicio++;float b=*inicio;
+    cout<<"\nlargo: "<<a;
+    cout<<"\nancho: "<<b;
+    cout<<"\nArea: "<<a*b<<"\n\n";
+    return 2;
 }
-void calcularArea(float (*pf)()){
-    cout<<"el area es : "<<pf();
+
+void calcularArea(int (*pf[])(int *)){
+    int r = 7,l = 10,t1 = 4,t2 = 5, t3 = 8,largoR = 2,anchoR = 6;
+    int datos[] = {r,l,t1,t2,t3,largoR,anchoR};
+    int *pdatos = datos;
+    cout<<sizeof(datos)/sizeof(*pdatos);
+    for(int i=0,j=0;i<sizeof(datos)/sizeof(pdatos);){
+        i+=(*pf)(pdatos);
+        for(;j<i;j++){pdatos++;}
+        pf++;
+    }   
 }
+
+
 
 int main(){
+    int (*pF[])(int*) = {circulo,cuadrado,triangulo,rectangulo};
+    calcularArea(pF);
 
-    calcularArea(&triangulo);
     return 0;
 }
